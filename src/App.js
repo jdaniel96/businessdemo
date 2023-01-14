43142b22cbc68e0f21251b18e949990a8555c034
J.Daniel <juandaniel9619@gmail.com>
2023-01-13 21:39:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { app } from "./libs/firebase";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 function App() {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/libs/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApgWeuPNQD_Ls0E0vd_AGkDOF8HzZE6aA",
+  authDomain: "businessdemo-9c772.firebaseapp.com",
+  projectId: "businessdemo-9c772",
+  storageBucket: "businessdemo-9c772.appspot.com",
+  messagingSenderId: "317485345268",
+  appId: "1:317485345268:web:69329a6518fa56ffad31a1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
